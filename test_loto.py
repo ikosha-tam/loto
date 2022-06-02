da01d6be53f24f70b6ea5b634e09aa29c2fce822
ikosha-tam <ikosha-tam@yandex.ru>
2022-06-02 15:53:29 +0300

    Add test_loto.py (work)

---
+++ test_loto.py
@@ -0,0 +1,16 @@
+import pytest
+from loto_class import Loto
+
+class TestLoto:
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_init_bag(self):
+        pass
+
+
+
