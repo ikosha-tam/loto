01580fa60b96bb5858d4fd9aaca94b57744af9bd
ikosha-tam <ikosha-tam@yandex.ru>
2022-06-02 20:59:56 +0300

    Update test_loto.py (home)

--- test_loto.py
+++ test_loto.py
@@ -1,16 +1,22 @@
 import pytest
+
+import loto_class
 from loto_class import Loto
 
 class TestLoto:
 
-    def setup(self):
+    def setUp(self):
         pass
+        # self.gamer = Loto()
 
-    def teardown(self):
+    def tearDown(self):
         pass
+        # del self.gamer
 
     def test_init_bag(self):
-        pass
+        # gamer_test = Loto()
+        loto_class.init_bag()
+        assert loto_class.bag = [1,2,3,4,5,6,7]
 
 
 
